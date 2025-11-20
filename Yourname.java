d20f84206c4bc692dc4b346693a575d4bee028af
Irbaz Ahmad <irbazamd714@gmail.com>
2025-11-20 12:50:01 +0545

    abcgws

---
+++ Yourname.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Yourname here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Yourname{
+    public static void main(String[]args){
+        System.out.println("Irbaz Ahmad");
+    }
+}
